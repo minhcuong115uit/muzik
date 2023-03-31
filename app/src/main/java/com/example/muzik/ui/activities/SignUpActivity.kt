c69d509ae12f86ef5a8467568a5ff0cef3def1b5
lancer <jbkhanhtran@gmail.com>
2023-03-31 16:15:44 +0700

    "Update - handle navigation with animation between sign up fragments"

--- app/src/main/java/com/example/muzik/ui/activities/SignUpActivity.kt
+++ app/src/main/java/com/example/muzik/ui/activities/SignUpActivity.kt
@@ -2,36 +2,41 @@ package com.example.muzik.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.NavHostFragment
 import com.example.muzik.R
-import com.example.muzik.databinding.ActivitySignInBinding
 import com.example.muzik.databinding.ActivitySignUpBinding
 import com.example.muzik.listeners.AuthListener
 import com.example.muzik.ui.fragments.SignUpPhase1
 import com.example.muzik.utils.ViewUtils
-import com.example.muzik.viewmodels.authentication.SignInViewModel
 import com.example.muzik.viewmodels.authentication.SignUpViewModel
 
 class SignUpActivity : AppCompatActivity(), AuthListener {
     private lateinit var  signUpViewModel: SignUpViewModel;
     private lateinit var  binding: ActivitySignUpBinding;
+    private lateinit var navController: NavController;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        signUpViewModel =  ViewModelProvider(this)[SignUpViewModel::class.java];
-        binding = DataBindingUtil.setContentView(this,R.layout.activity_sign_up);
+        signUpViewModel =  ViewModelProvider(this)[SignUpViewModel::class.java]
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_sign_up)
         signUpViewModel.setAuthListener(this)
-        binding.viewmodel = signUpViewModel;
+        binding.viewmodel = signUpViewModel
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+        signUpViewModel.setNavController((navController));
         setContentView(binding.root)
 
-        val fragmentManager: FragmentManager = supportFragmentManager
-        val transaction = fragmentManager.beginTransaction()
-        val bundle = Bundle()
-
-        val fragment = SignUpPhase1();
-        transaction.add(binding.signUpFragmentContainer.id, fragment)
-        transaction.commit()
+//        val fragmentManager: FragmentManager = supportFragmentManager
+//        val transaction = fragmentManager.beginTransaction()
+//        val fragment = SignUpPhase1()
+//        transaction.add(R.id.nav_host_fragment, fragment)
+//        transaction.commit()
     }
 
     override fun onStarted() {
@@ -45,4 +50,5 @@ class SignUpActivity : AppCompatActivity(), AuthListener {
     override fun onFailure(message: String?) {
         ViewUtils.showToast(this,message);
     }
+
 }
--- app/src/main/java/com/example/muzik/viewmodels/authentication/SignUpViewModel.kt
+++ app/src/main/java/com/example/muzik/viewmodels/authentication/SignUpViewModel.kt
@@ -4,8 +4,13 @@ import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.navigation.NavController
+import com.example.muzik.R
+import com.example.muzik.data.models.User
 import com.example.muzik.data.repositories.AuthFirebaseRepository
+import com.example.muzik.data.repositories.UserRepository
 import com.example.muzik.listeners.AuthListener
+import com.example.muzik.ui.fragments.SignUpPhase1
 import com.example.muzik.utils.FirebaseAuthManager
 import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.auth.AuthResult
@@ -14,22 +19,54 @@ import com.google.firebase.auth.FirebaseUser
 
 class SignUpViewModel (savedStateHandle: SavedStateHandle): ViewModel() {
     private var firebaseAuth: FirebaseAuth = FirebaseAuthManager.getInstance();
-
+    private lateinit var navController: NavController
+    private var authListener: AuthListener? = null
+    //user data
     var email: MutableLiveData<String> = MutableLiveData();
     var password : MutableLiveData<String> = MutableLiveData();
-    private var authListener: AuthListener? = null
+    var firstName: MutableLiveData<String> = MutableLiveData();
+    var lastName : MutableLiveData<String> = MutableLiveData();
+    var age: MutableLiveData<String> = MutableLiveData();
+    var gender : MutableLiveData<String> = MutableLiveData();
+    var displayName : MutableLiveData<String> = MutableLiveData();
 
+    //
+    val ages = listOf("18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40")
+    fun setNavController(controller: NavController) {
+        navController = controller
+    }
     private fun handleSignUp (email: String?, password:String?){
         if(email.isNullOrEmpty() || password.isNullOrEmpty())
             return;
         AuthFirebaseRepository.instance?.signUpWithEmail(email,password,this.authListener!!);
+        handleCreateUser();
+
     }
 
-    fun onSignUpWithEmailClicked () {
-        handleSignUp(email.value,password.value);
+
+
+    fun handleCreateUser () {
+
+//        val user:User = User(
+//            firstName.value,lastName.value, displayName.value,
+//            gender.value,age.value, official = false)
+//
+//        UserRepository.instance.createUser();
+
     }
 
     fun setAuthListener(authListener: AuthListener) {
         this.authListener = authListener
     }
+    fun handleNavigateToSignUpPhase2(){
+        navController.navigate(R.id.action_signUpPhase1_to_signUpPhase2);
+    }
+    fun handleNavigateToSignUpPhase3(){
+        navController.navigate(R.id.action_signUpPhase2_to_signUpPhase3);
+    }
+    fun onAgeChanged() {
+        Log.d("ViewModel - AgeChange","Age Changed")
+    }
+
+
 }
