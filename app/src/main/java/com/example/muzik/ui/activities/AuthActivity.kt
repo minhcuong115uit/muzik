15e63f7a3dcfeaf443f4064abc0318b42b91bdea
lancer <jbkhanhtran@gmail.com>
2023-04-14 12:42:24 +0700

    Create UI of music player

---
+++ app/src/main/java/com/example/muzik/ui/activities/AuthActivity.kt
@@ -0,0 +1,12 @@
+package com.example.muzik.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.muzik.R
+
+class AuthActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auth)
+    }
+}
---
+++ app/src/main/java/com/example/muzik/ui/activities/WelcomeActivity.kt
@@ -0,0 +1,34 @@
+package com.example.muzik.ui.activities
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import com.example.muzik.R
+import com.example.muzik.databinding.ActivityWelcomeBinding
+
+class WelcomeActivity : AppCompatActivity()  {
+    private lateinit var binding: ActivityWelcomeBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityWelcomeBinding.inflate(layoutInflater);
+        setContentView(binding.root);
+
+        val createAccountBtn = binding.btnCreateAccount;
+        val haveAnAccountBtn = binding.btnAlreadyHaveAccount
+
+        createAccountBtn.setOnClickListener(View.OnClickListener {
+            val intent = Intent(this, SignUpActivity::class.java);
+            startActivity(intent);
+        })
+        haveAnAccountBtn.setOnClickListener(View.OnClickListener {
+            val intent = Intent(this, SignInActivity::class.java);
+            startActivity(intent);
+        })
+    }
+
+
+}
